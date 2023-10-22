d12608cf8fa362b76034cc532fb0d316323d5de4
Alejandro <alejoramvids@gmail.com>
2023-10-22 14:24:40 -0600

    Resolví los ejercicios completos

--- PAT_HojaDeTrabajo02/Ejercicio01.h
+++ PAT_HojaDeTrabajo02/Ejercicio01.h
@@ -5,6 +5,31 @@
 class Ejercicio01
 {
 public:
-	Node<char>* rotateRight(Node<char>* head, int k);
+	Node<char>* rotateRight(Node<char>* head, int k) {
+		if (head == nullptr || head->next == nullptr || k == 0) {
+			return head;
+		}
+
+		Node<char>* old_tail = head;
+		int len = 1;
+		while (old_tail->next != nullptr) {
+			old_tail = old_tail->next;
+			len++;
+		}
+
+		old_tail->next = head;
+
+		Node<char>* new_tail = head;
+		for (int i = 0; i < len - k % len - 1; i++) {
+			new_tail = new_tail->next;
+		}
+
+		Node<char>* new_head = new_tail->next;
+
+		new_tail->next = nullptr;
+
+		return new_head;
+	}
 };
+
 
--- PAT_HojaDeTrabajo02/Ejercicio02.h
+++ PAT_HojaDeTrabajo02/Ejercicio02.h
@@ -2,9 +2,24 @@
 
 #include "Node.h"
 
-class Ejercicio02
-{
+class Ejercicio02 {
 public:
-	Node<int>* addTwoNumbers(Node<int>* l1, Node<int>* l2);
+    Node<int>* addTwoNumbers(Node<int>* l1, Node<int>* l2) {
+        Node<int> dummy(0);
+        Node<int>* current = &dummy;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum = carry + (l1 ? l1->value : 0) + (l2 ? l2->value : 0);
+            carry = sum / 10;
+            current->next = new Node<int>(sum % 10);
+            current = current->next;
+
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+
+        return dummy.next;
+    }
 };
 
